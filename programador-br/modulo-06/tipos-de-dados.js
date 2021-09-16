8fb3d4dabc3b520b85ff011015b896ddd6f47436
nely de faria <nelly.nf54@gmail.com>
2021-09-16 15:43:39 -0300

    Criando arquivo tipos-de-dados.js

---
+++ programador-br/modulo-06/tipos-de-dados.js
@@ -0,0 +1,17 @@
+// boolean
+// null
+// undedined
+// number
+// String
+
+let nome = "nelynely";
+console.log(nome);
+
+let number = 1;
+console.log(number);
+
+let ligado = true;
+console.log(ligado);
+
+let a;
+console.log(a);
