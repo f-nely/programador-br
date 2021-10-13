0fe928550526ad2c129f057d8d696c583fc77b0a
nely de faria <nelly.nf54@gmail.com>
2021-10-13 17:45:58 -0300

    Criando arquivo operadocoes-aritmeticas.js

---
+++ programador-br/modulo-06/operadocoes-aritmeticas.js
@@ -0,0 +1,12 @@
+var a = 10;
+var b = 15;
+
+var soma = a + b;
+var subtracao = a - b;
+var multiplicacao = a * b;
+var divisao = a / b;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
