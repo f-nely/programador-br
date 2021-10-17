1b6a63bbe30dc5ee89f5ca84448a5106d9ac1a3d
nely de faria <nelly.nf54@gmail.com>
2021-10-17 16:51:30 -0300

    Criando arquivo condicionais-if.js

---
+++ programador-br/modulo-06/condicionais-if.js
@@ -0,0 +1,12 @@
+var idade = 47;
+
+if(idade >= 35) {
+    console.log("pode");
+    console.log("Qual o seu pedido?");
+} else if(idade >= 18) {
+    console.log("pode");
+    console.log("Mostre a identidade");
+} else {
+    console.log("não pode");
+    console.log("Volte futuramente")
+}
