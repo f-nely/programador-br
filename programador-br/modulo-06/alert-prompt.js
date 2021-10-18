c3037be9264095981cb5a2780e4cbd8b10fd087a
nely de faria <nelly.nf54@gmail.com>
2021-10-18 18:10:42 -0300

    Criando arquivo alert-prompt.js

---
+++ programador-br/modulo-06/alert-prompt.js
@@ -0,0 +1,19 @@
+
+//alert("Boa tarde!");
+//var nome = prompt("Qual o seu nome?");
+//alert(`Boa tarde, ${nome}!`)
+
+//var num1 = parseInt(prompt("Digite um número"));
+//var num2 = parseInt(prompt("Digite outro número"));
+
+//var soma = num1 + num2;
+
+//alert(`A soma entre ${num1} e ${num2} = ${soma}`);
+
+var numero = parseInt(prompt("Digite um número"));
+
+var dobro = numero * 2;
+
+alert(`O dobro de ${numero} é ${dobro}`);
+
+
