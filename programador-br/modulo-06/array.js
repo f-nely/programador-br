b69956adbcd7ad1bf44e685145c1308e81183b04
nely de faria <nelly.nf54@gmail.com>
2021-10-20 17:12:40 -0300

    Alterando for

--- programador-br/modulo-06/array.js
+++ programador-br/modulo-06/array.js
@@ -2,6 +2,10 @@ let students = ["Igor", "James", "Diana", "Lua"];
 
 //console.log(students[0]);
 
-for(let i = 0; i < students.length; i++) {
+for(let i in students) {
     console.log(students[i]);
+}
+
+for(let student of students) {
+    console.log(student);
 }
