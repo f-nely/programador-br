03fe4ec2bf66d4e9d456a6b57a21edc3acbd9098
nely de faria <nelly.nf54@gmail.com>
2021-10-23 19:45:03 -0300

    Criando arquivo funcao.js

---
+++ programador-br/modulo-06/funcao.js
@@ -0,0 +1,16 @@
+function calcularMedia(nota1, nota2) {
+    let media = (nota1 + nota2) / 2;
+    return media;
+}
+
+console.log("A média é: " + calcularMedia(10, 8.5));
+
+let media = function(n1, n2) {
+    return (n1 + n2) / 2;
+}
+
+console.log(media(5, 6));
+
+let calcMedia = (num1, num2) => {
+    return (num1 + num2) / 2;
+}
