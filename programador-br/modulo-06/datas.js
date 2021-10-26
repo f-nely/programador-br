e74c54bf4b3b225539638b0f7072796e79d07747
nely de faria <nelly.nf54@gmail.com>
2021-10-26 12:42:32 -0300

    Criando arquivo datas.js

---
+++ programador-br/modulo-06/datas.js
@@ -0,0 +1,17 @@
+
+/* let d = new Date();
+console.log(d); */
+
+/* let d = new Date(2018, 08, 12, 22, 45);
+console.log(d); */
+
+/* let d = new Date(10000);
+console.log(d); */
+
+/* let d = new Date("sep 05 2017 23:25");
+console.log(d); */
+
+let d = new Date("05-15-1999");
+console.log(d.getDate());
+console.log(d.getFullYear());
+console.log(d);
