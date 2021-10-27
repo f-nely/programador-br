ee366e5934202dfdb2c391c336e5b547187252d0
nely de faria <nelly.nf54@gmail.com>
2021-10-27 18:33:12 -0300

    Criando arquivo document.js

---
+++ programador-br/modulo-07/document.js
@@ -0,0 +1,4 @@
+console.log(document);
+//console.log(document.body);
+//console.log(document.body.innerHTML);
+document.body.innerHTML = "<h1>Esse título foi inserido no JavaScript" + document.body.innerHTML;
