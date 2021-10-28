0e2b9cf9a9adab1d21d80425dbce7f1669d1ebda
nely de faria <nelly.nf54@gmail.com>
2021-10-28 16:54:05 -0300

    Adicionando comentário

--- programador-br/modulo-07/document.js
+++ programador-br/modulo-07/document.js
@@ -1,4 +1,4 @@
 console.log(document);
 //console.log(document.body);
 //console.log(document.body.innerHTML);
-document.body.innerHTML = "<h1>Esse título foi inserido no JavaScript" + document.body.innerHTML;
+/* document.body.innerHTML = "<h1>Esse título foi inserido no JavaScript" + document.body.innerHTML; */
