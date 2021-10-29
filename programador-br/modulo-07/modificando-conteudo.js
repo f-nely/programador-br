de2021588cb55fa58cf11aaf53b10691f036b9d6
nely de faria <nelly.nf54@gmail.com>
2021-10-29 19:43:53 -0300

    Criando arquivo modificando-conteudo.js

---
+++ programador-br/modulo-07/modificando-conteudo.js
@@ -0,0 +1,9 @@
+let example1 = document.getElementById("p1");
+//example1.innerHTML = "Novo conteúdo";
+
+let example2 = document.getElementById("p1");
+//example2.innerText = "Conteúdo mais novo";
+
+let example3 = document.getElementById("p1");
+example3.textContent = "Mais novo ainda";
+example3.style = "color: red";
